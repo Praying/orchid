7887f29230978dbf3774f6564b34823073c71138
Praying <snowfallvilla@163.com>
2020-08-25 10:49:22 +0800

    remove unused import

--- proto/build.rs
+++ proto/build.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
 fn main() {
     let path = std::path::Path::new("src/proto");
-    if path.exists(){
+    if path.exists() {
         tonic_build::configure()
             .out_dir(&path)
-            .compile(&["counter/counter.proto","raft/raft.proto"], &["counter", "raft"])
+            .compile(
+                &["counter/counter.proto", "raft/raft.proto"],
+                &["counter", "raft"],
+            )
             .unwrap();
-
     }
-
 }
