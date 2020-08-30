336f84d8fafd8d62b653150062c87db82636570a
Praying <snowfallvilla@163.com>
2020-08-30 16:44:32 +0800

    try to use tonic

--- examples/counter/client/counter_client.rs
+++ examples/counter/client/counter_client.rs
@@ -1,5 +1,18 @@
+use orchid_proto::proto::raft_pb::raft_pb_server::RaftPb;
+use orchid_proto::proto::raft_pb::{
+    raft_pb_client::RaftPbClient, raft_pb_server::RaftPbServer, VoteRequest, VoteResponse,
+};
+use std::mem::MaybeUninit;
 
+use tonic::{transport::Endpoint, transport::Server, Request, Response, Status};
+use orchid::node::raft_node::RaftService;
 
-fn main() {
-
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let channel = Endpoint::from_static("http://[::1]:50056").connect().await?;
+    let mut raft_client = RaftPbClient::new(channel.clone());
+    let request = tonic::Request::new(VoteRequest {});
+    let response = raft_client.vote(request).await?;
+    println!("vote response = {:?}", response.get_ref().num);
+    Ok(())
 }
--- src/node/raft_node.rs
+++ src/node/raft_node.rs
@@ -1,10 +1,9 @@
-use futures::task::SpawnExt;
 use orchid_proto::proto::raft_pb::raft_pb_server::RaftPb;
 use orchid_proto::proto::raft_pb::{
     raft_pb_client::RaftPbClient, raft_pb_server::RaftPbServer, VoteRequest, VoteResponse,
 };
 use std::mem::MaybeUninit;
-use std::net::SocketAddr;
+
 use tonic::{transport::Endpoint, transport::Server, Request, Response, Status};
 
 enum State {
@@ -32,35 +31,44 @@ impl RaftNode {
         }
     }
     async fn start(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        // let channel = Endpoint::from_static(format!("http://{}:{}",&self.address,&self.port).as_str());
-        // self.client = Box::new(channel);
+        //let channel = Endpoint::from_static(format!("http://{}:{}",&self.address,&self.port).as_str());
+        //self.client = Box::new(channel);
         Ok(())
     }
 }
-
-pub struct MyRaftPB {}
+#[derive(Default)]
+pub struct RaftService {}
 
 #[tonic::async_trait]
-impl RaftPb for MyRaftPB {
+impl RaftPb for RaftService {
     async fn vote(&self, _reqeust: Request<VoteRequest>) -> Result<Response<VoteResponse>, Status> {
-        let reply = VoteResponse {};
+        let reply = VoteResponse { num: 20 };
+
         Ok(Response::new(reply))
     }
 }
+
+#[macro_export]
 macro_rules! aw {
     ($e:expr) => {
         tokio_test::block_on($e)
     };
 }
+
 #[test]
 fn test_create_server() {
-    // let channel = tonic::transport::Endpoint::from_static("");
-    // let addr: SocketAddr = "[::1]:50051".parse().unwrap();
-    // let raft_service = MyRaftPB {};
+    // let addr = "[::1]:50051".parse().unwrap();
+    // let raft_service = RaftPbServer::new(RaftService::default());
+    // std::thread::spawn(|| {
+    //     std::thread::sleep(std::time::Duration::from_secs(1));
+    //     let channel = aw!(Endpoint::from_static("http://[::1]:50051").connect()).unwrap();
+    //     let mut raft_client = RaftPbClient::new(channel.clone());
+    //     let request = tonic::Request::new(VoteRequest {});
+    //     let response = aw!(raft_client.vote(request)).unwrap();
+    //     println!("vote response = {:?}", response.get_ref().num);
+    // });
+    // aw!(Server::builder().add_service(raft_service).serve(addr));
 }
 
 #[test]
-fn test_create_client() {
-    // let mut node = RaftNode::new("127.0.0.1".into(), 8089);
-    // aw!(node.start());
-}
+fn test_create_client() {}
